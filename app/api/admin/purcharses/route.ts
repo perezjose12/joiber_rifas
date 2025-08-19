0a8b9f74c31c5d22cea3aa1759f1faf77c51ba4d
RohyllerP <198981260+perezjose12@users.noreply.github.com>
2025-08-19 22:51:07 +0100

    actualizado error de purcharse

--- app/api/admin/purcharses/route.ts
+++ app/api/admin/purcharses/route.ts
@@ -1,6 +1,6 @@
 import { supabaseServer } from "@/lib/supabaseServer";
 import { getServerSession } from "next-auth/next";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { authOptions } from "@/lib/auth";
 type Purchase = {
   id: number;
   tickets: number;
